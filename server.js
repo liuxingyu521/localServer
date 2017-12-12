0752ea3f4a935f9afa9b7604312ebbdc747fdf5d
liuxingyu521 <xuj_power@qq.com>
2017-12-12 20:39:14 +0800

    add: 新增express中间件body-parser，来解析post请求体

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var http = require('http');
 
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+
+// 使用body-parser插件来解析post请求的request body
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ type: 'application/*+json' }));
 
 // server端口
 var httpPort = 10080;
@@ -11,9 +17,17 @@ var httpServer = http.createServer(app);
 
 
 // 使用express（相当于中间件）处理请求
-app.get('/', function(req, res){
-		res.status(200);
-		res.send('welcome to http server');
+app.get('/get', function(req, res){
+	res.status(200);
+	res.send('welcome to http server');
+	red.end();
+})
+
+// post 请求
+app.post('/post', function(req, res){
+	// 使用req.body来解析请求体
+	res.status(200).send(req.body);
+	res.end();
 })
 
 // 使用server监听端口
