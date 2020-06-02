a6d5148bf6aa2330104105a521ac05dfd40b7870
liuxingyu521 <xuj_power@qq.com>
2020-06-02 19:09:37 +0800

    feat: 添加 nodemon,自动重启 server

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const http = require('http');
-const axios = require('axios');
-const express = require('express');
-const bodyParser = require('body-parser');
-const Middlewares = require('./serverMiddleware')
+const http = require("http");
+const axios = require("axios");
+const express = require("express");
+const bodyParser = require("body-parser");
+const Middlewares = require("./serverMiddleware");
 
 const app = express();
 
 // 使用body-parser插件来解析post请求的request body
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.json({ type: "application/*+json" }));
 // 解决跨域问题
-app.use(Middlewares.CrossAssign())
+app.use(Middlewares.CrossAssign());
 
 // server端口
 var httpPort = 10080;
@@ -18,35 +18,32 @@ var httpPort = 10080;
 // 创建Server
 var httpServer = http.createServer(app);
 
-
 // 使用express（相当于中间件）处理请求
-app.get('/get', function(req, res){
-	res.status(200);
-	res.send('welcome to http server');
-	res.end();
-})
+app.get("/get", function (req, res) {
+  res.status(200);
+  res.send("welcome to http server");
+  res.end();
+});
 
 // post 请求
-app.post('/post', function(req, res){
-	// 使用req.body来解析请求体
-	res.status(200).send(req.body);
-	res.end();
-})
+app.post("/post", function (req, res) {
+  // 使用req.body来解析请求体
+  res.status(200).send(req.body);
+  res.end();
+});
 
 // 代理请求其他接口数据
-app.get('/getPersonInfo', function (req, res) {
+app.get("/getPersonInfo", function (req, res) {
   axios({
-      url: 'http://abc.com/getPersonInfo',
-      method: 'get'
-    })
-    .then(function (response) {
-      res.status(200).send(response.data);
-      res.end();
-    })
-
-})
+    url: "http://abc.com/getPersonInfo",
+    method: "get",
+  }).then(function (response) {
+    res.status(200).send(response.data);
+    res.end();
+  });
+});
 
 // 使用server监听端口
-httpServer.listen(httpPort, function(){
-	console.log('http-server lauched at http://localhost:%s', httpPort);
-})
+httpServer.listen(httpPort, function () {
+  console.log("http-server lauched at http://localhost:%s", httpPort);
+});
